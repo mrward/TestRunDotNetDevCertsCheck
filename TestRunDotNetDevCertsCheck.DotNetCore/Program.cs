d2dd5496f3d5fe2c5f9ed086c37219c0b6adfe60
Matt Ward <matt.ward@microsoft.com>
2021-07-19 12:24:24 +0100

    Add console test app

---
+++ TestRunDotNetDevCertsCheck.DotNetCore/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TestRunDotNetDevCertsCheck.DotNetCore
+{
+	class Program
+	{
+		public static void Main (string[] args)
+		{
+			try {
+				Run ();
+			} catch (Exception ex) {
+				Console.WriteLine (ex);
+			}
+		}
+
+		/// <summary>
+		/// This works fine.
+		/// </summary>
+		static void Run ()
+		{
+			using (var process = new Process ()) {
+
+				var info = new ProcessStartInfo ();
+				info.FileName = "/usr/local/share/dotnet/dotnet";
+				info.Arguments = "dev-certs https --trust --check";
+
+				process.StartInfo = info;
+				process.Start ();
+				process.WaitForExit ();
+
+				int exitCode = process.ExitCode;
+				Console.WriteLine (exitCode);
+			}
+		}
+	}
+}
